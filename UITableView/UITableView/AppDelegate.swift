d8238ae73ddba757aa513cff5564cd9052aa7f6c
Gabriel Sanzone <gabriel.sanzone@fcamara.com.br>
2019-07-23 16:43:52 -0300

    ep01
    https://www.youtube.com/watch?v=VFtsSEYDNRU

--- UITableView/UITableView/AppDelegate.swift
+++ UITableView/UITableView/AppDelegate.swift
@@ -15,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        let navController = UINavigationController(rootViewController: ViewController())
+        //Definir qual a classe controle
+        window?.rootViewController = navController
+        
         return true
     }
 
--- UITableView/UITableView/ViewController.swift
+++ UITableView/UITableView/ViewController.swift
@@ -8,11 +8,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    let cellId = "cellId123456"
+    
+    //Array de nomes
+    let names = [
+        "Amy", "Bill", "Zelda", "Gabriel"
+    ]
+    
+    let nomesComC = [
+        "Carlos", "Carina", "Caliu", "Calopsita"
+    ]
+    
+    let arrayDimencional = [
+        ["Amy", "Bill", "Zelda", "Gabriel"],
+        ["Carlos", "Carina", "Caliu", "Calopsita"],
+        ["Daniel", "Dadinho"],
+        ["Patrick", "Pato"]
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //define cor para a tela
+        view.backgroundColor = UIColor.white
+        //Define o titulo
+        navigationItem.title =  "Contatos"
+        //Define titulo Grande IOS < 10
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.text = "Header"
+        label.backgroundColor = UIColor.lightGray
+        return label
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return arrayDimencional.count
+    }
+    
+    
+    //definir numeros de linhas da tabela
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return 10
+        //Para retornar o numero de linhas nescessarios para o Array
+        //return nomesComC.count
+        return arrayDimencional[section].count
+    }
+    
+    //retornar as celulas
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        //pega valores da array
+        //let name = self.names[indexPath.row]
+        
+        //let name = indexPath.section == 0 ? names[indexPath.row] : nomesComC[indexPath.row]
+        
+        let name = arrayDimencional[indexPath.section][indexPath.row]
+        //Texto que terá nas celulas
+        cell.textLabel?.text = name
+        
+        cell.textLabel?.text = "\(name) Section:\(indexPath.section) Row:\(indexPath.row)"
+        
+        return cell
     }
 
 
