30e279dc17402adcaf92005edb4aac085eababd1
Gabriel Sanzone <gabriel.sanzone@fcamara.com.br>
2019-07-23 10:03:35 -0300

    0.0.0.0
    Ciração inicial

---
+++ UITableView/UITableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableView
+//
+//  Created by Gabriel Chirico Mahtuk de Melo Sanzone on 23/07/19.
+//  Copyright © 2019 Gabriel Chirico Mahtuk de Melo Sanzone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
